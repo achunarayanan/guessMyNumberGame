70b8cda478b78aaf8dfdba432637191f141bf4bf
Achu Narayanan <116958125+achunarayanan@users.noreply.github.com>
2023-02-20 01:53:34 +0530

    add files via upload

---
+++ script.js
@@ -0,0 +1,93 @@
+"use strict";
+
+let secretNumber = Math.floor(Math.random() * 20) + 1;
+
+console.log(secretNumber);
+
+let score = 20;
+let heighScore = 0;
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guessNumber = Number(document.querySelector(".guess").value);
+
+  if (!guessNumber) {
+    document.querySelector(".message").textContent = "No number!";
+    let audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+  } else if (guessNumber === secretNumber) {
+    document.querySelector(".message").textContent = "Correct Answer";
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
+    let audio = new Audio("sounds/win.mp3");
+    audio.play();
+
+    if (score > heighScore) {
+      heighScore = score;
+      document.querySelector(".highscore").textContent = heighScore;
+    }
+  } else if (guessNumber !== secretNumber) {
+    if (score > 1) {
+      document.querySelector(".message").textContent =
+        guessNumber > secretNumber ? "Too heigh" : "Too low";
+      score--;
+      document.querySelector(".score").textContent = score;
+    } else {
+      document.querySelector(".message").textContent = "You lost the game";
+      document.querySelector(".score").textContent = 0;
+      document.querySelector("body").style.backgroundColor = "red";
+      let audio = new Audio("sounds/wrong.mp3");
+      audio.play();
+    }
+  }
+});
+
+// } else if (guessNumber > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector(".message").textContent = "Too heigh";
+//     score--;
+//     document.querySelector(".score").textContent = score;
+//   } else {
+//     document.querySelector(".message").textContent = "You lost the game";
+//     document.querySelector(".score").textContent = 0;
+//     document.querySelector("body").style.backgroundColor = "red";
+//     let audio = new Audio('sounds/wrong.mp3');
+//     audio.play();
+//   }
+// } else if (guessNumber < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector(".message").textContent = "Too low";
+//     score--;
+//     document.querySelector(".score").textContent = score;
+//   } else {
+//     document.querySelector(".message").textContent = "You lost the game";
+//     document.querySelector(".score").textContent = 0;
+//     document.querySelector("body").style.backgroundColor = "red";
+//     let audio = new Audio('sounds/wrong.mp3');
+//     audio.play();
+//   }
+// }
+
+//   }
+// });
+
+// play Again
+document.querySelector(".again").addEventListener("click", function () {
+  // resetting score
+  score = 20;
+  document.querySelector(".score").textContent = score;
+
+  // resetting secretNumber
+  secretNumber = Math.floor(Math.random() * 20) + 1;
+ 
+  // resetting message
+  document.querySelector(".message").textContent = "Start guessing...";
+
+  // resetting inputfield
+  document.querySelector(".guess").value = "";
+
+  // resetting styles
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".number").textContent = "?";
+});
